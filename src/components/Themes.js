a1e071dd3f28369031e3f0a30d327583cc789f2a
athipratkanha <athiprat.kanha@gmail.com>
2022-08-22 17:30:03 +0700

    reactportfolio

---
+++ src/components/Themes.js
@@ -0,0 +1,19 @@
+// Here we will create dark and light theme variables
+
+export const lightTheme = {
+
+    body:"#F2EFDF",
+    text:"#40352C",
+    fontFamily:"'Source Sans Pro', sans-serif",
+    bodyRgba : "252, 246, 244",
+    textRgba:"0,0,0",
+}
+
+export const DarkTheme = {
+
+    body:"#081826",
+    text:"#30F2F2",
+    fontFamily:"'Source Sans Pro', sans-serif",
+    textRgba : "252, 246, 244",
+    bodyRgba:"0,0,0",
+}
---
+++ src/data/WorkData.js
@@ -0,0 +1,44 @@
+// work data
+
+export const Work = [
+    {
+        id:1,
+        name:"Colmar",
+        description:"It is build on HTML5, CSS. It have responsive. ",
+        tags:["HTML5","CSS"],
+        
+        demo:"https://colmarauto.netlify.app/",
+        github:"https://github.com/AthipratKanha/AthipratKanha.github.io/tree/main/colmar"
+    },
+    {
+        id:2,
+        name:"Jamming",
+        description:"Add playlist to Spotify",
+        tags:["React"],
+        
+        demo:"http://autojamming.surge.sh/",
+        github:"https://github.com/AthipratKanha/Jamming/tree/main/Jamming"
+        
+    },
+    {
+        id:3,
+        name:"Find Your Hat",
+        description:"Mini Game",
+        tags:["Javascript"],
+        
+        demo:"https://replit.com/@AutoAthiprat/Find-Your-Hat-Test#index.js",
+        github:"https://github.com/AthipratKanha/Find-Your-Hat/tree/main/Find%20Your%20Hat"
+        
+    },
+    {
+        id:4,
+        name:"The Tea Cozy",
+        description:"HTML and Flex CSS ",
+        tags:["HTML5", "CSS"],
+        
+        demo:"https://teacozyauto.netlify.app/",
+        github:"https://github.com/AthipratKanha/TeaCozy"
+        
+    }
+  
+] 
